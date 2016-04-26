99c96b56726cc3e953e8bfa64d285014c87c4bbb
zerolocker <zero-lzd@163.com>
2016-04-25 20:54:26 -0500

    add quantative evaluation data

---
+++ evaldata/draw4BoundingBoxes.m
@@ -0,0 +1,22 @@
+function draw4BoundingBoxes(filename)
+    [~,image_name,~] = fileparts(filename);
+    I = imread(filename);
+    figure, imshow(I);
+    prompts = {'Draw 4 face bounding boxes (try to cover the margin of the face as close as possible)'};
+    rects = [];
+    for i = 1:6
+        title(prompts{1});
+        rect = int32(getrect);
+        rects = [rects;rect];
+        hold on;
+        rectangle('Position',rect,'EdgeColor',[1 1 0])
+    end
+    
+    fout = fopen(sprintf('%s.rect.txt',image_name),'w');
+    for i = 1:size(rects,1)
+        fprintf(fout,'%d ',rects(i,:));
+        fprintf(fout,'\n');
+    end
+    fclose(fout);
+    title(sprintf('Done! File saved to %s.rect.txt',image_name));
+end
